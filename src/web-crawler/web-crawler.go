4b2e40d1cc0d3ac16f225fdf5fd235f6cfb2435b
WeichenXu <weichenxu1993@icloud.com>
2017-10-29 14:52:08 -0400

    web-crawler with mutex&waitgroup

--- src/web-crawler/web-crawler.go
+++ src/web-crawler/web-crawler.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+// 1. Use mutex and wait group
+// 2. Use golang channel
 
 type Fetcher interface {
 	// Fetch returns the body of URL and
@@ -10,11 +14,10 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+/* Serialized */
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
+func CrawlSerialized(url string, depth int, fetcher Fetcher) {
 	// This implementation doesn't do either:
 	if depth <= 0 {
 		return
@@ -26,13 +29,62 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	}
 	fmt.Printf("found: %s %q\n", url, body)
 	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+		CrawlSerialized(u, depth-1, fetcher)
 	}
 	return
 }
 
+/* Parallel with mutex*/
+type fetchState struct {
+	m_mutex  sync.Mutex
+	m_states map[string]bool
+}
+
+func makeFetchState() *fetchState {
+	f := &fetchState{}
+	f.m_states = make(map[string]bool)
+	return f
+}
+
+func (f *fetchState) checkState(url string) bool {
+	//f.m_mutex.Lock()
+	//defer f.m_mutex.Unlock()
+	if f.m_states[url] {
+		return true
+	}
+	f.m_states[url] = true
+	return false
+}
+
+func CrawlMutex(url string, depth int, fetcher Fetcher, f *fetchState) {
+	if depth < 0 || f.checkState(url) {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	var wg sync.WaitGroup
+	for _, u := range urls {
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+			CrawlMutex(u, depth-1, fetcher, f)
+		}(u)
+	}
+	wg.Wait()
+	return
+}
+
 func main() {
-	Crawl("http://golang.org/", 4, fetcher)
+	fmt.Printf("Crawl with serialization\n")
+	CrawlSerialized("http://golang.org/", 4, fetcher)
+	fmt.Printf("--------------------\n")
+	fmt.Printf("Crawl parallel with mutex&waitGroup\n")
+	CrawlMutex("http://golang.org/", 4, fetcher, makeFetchState())
+	fmt.Printf("--------------------\n")
 }
 
 // fakeFetcher is Fetcher that returns canned results.
